bc494406aeba277c18ba65fdc48cb866fa860907
Ignacio Toledo <ignacio@ign.uy>
2024-12-11 11:16:02 -0300

    minor footer update

--- themes/tailpress-master/footer.php
+++ themes/tailpress-master/footer.php
@@ -1,18 +1,17 @@
-
 </main>
 
-<?php do_action( 'tailpress_content_end' ); ?>
+<?php do_action('tailpress_content_end'); ?>
 
 </div>
 
-<?php do_action( 'tailpress_content_after' ); ?>
+<?php do_action('tailpress_content_after'); ?>
 
-<footer id="colophon" class="site-footer bg-gray-50 py-12" role="contentinfo">
-	<?php do_action( 'tailpress_footer' ); ?>
+<footer id="colophon" class="site-footer bg-neutral-950 py-12" role="contentinfo">
+  <?php do_action('tailpress_footer'); ?>
 
-	<div class="container mx-auto text-center text-gray-500">
-		&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
-	</div>
+  <div class="container mx-auto text-center text-neutral-200">
+    &copy; <?php echo date_i18n('Y'); ?> - <?php echo get_bloginfo('name'); ?>
+  </div>
 </footer>
 
 </div>
@@ -20,4 +19,5 @@
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
